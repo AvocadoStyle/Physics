a7f2bbdc9c5a47c18d5bd7bcb16d2af4fe349103
AvocadoStyle <edenraf@hotmail.com>
2021-09-19 14:36:40 +0300

    last fixs all work untill kepler, need to add parsec units

--- Equations.py
+++ Equations.py
@@ -1,20 +1,18 @@
 from typing import Final
 import Units
 
-
+# value is performing distance
+# time = distance/speed
 class EquationUtil(Units.UnitUtil):
-    def __init__(self, type_val, value, time, distance):
+    def __init__(self, type_val, value, time=None):
         super().__init__(type_val, value)
-        self.__Time = time
-        self.__Distance = distance
-        self.LS: Final = 300000
+        self.__Time = time      #time in years
+        self.LS: Final = 300000 #light speed 300,000km per sec
+        self.AU: Final = 150000000
         # self.__unit = Units.UnitUtil(type_val, value)      #composition
 
     def getTime(self):
         return self.__Time
-
-    def getDistance(self):
-        return self.__Distance
 
     def getTimeOfLightSpeedInSecondsFromTheSun(self):
            return self.getKM() / (self.LS)
@@ -26,14 +24,22 @@ class EquationUtil(Units.UnitUtil):
            return self.getKM() / (self.LS * 60 * 60)
 
     def getKepler(self):
-        TimeSquar = self.__Time*self.__Time
-        DistanceThird = self.__Distance/self.getVal()
-        # DistanceThird = DistanceThird*DistanceThird*DistanceThird ####@@@@@@@@@todo@@@@@@@@@@@#####
-        K = TimeSquar/DistanceThird
-        return K
+        if not(self.__Time and self.getType() == "KM"):
+            print("ERROR - Time is None or type is not KM")
+            exit(0)
+        tSquere = self.__Time * self.__Time
+        radiusAU = self.getVal() / self.AU
+        radiusAuThird = radiusAU * radiusAU * radiusAU
+        return tSquere/radiusAuThird
 
     def getKeplerBoolean(self):
         k = self.getKepler()
         if k != 1:
             return False
         return True
+
+    def setTime(self, time):
+        self.__Time = time
+
+    def setVal(self, time):
+        self.__Time = time
--- main.py
+++ main.py
@@ -4,61 +4,6 @@ import matplotlib.pyplot as plt
 import astropy
 import numpy as np
 if __name__ == "__main__":
-    a = EquationUtil("AU", 5.2, 1, 1)
-    # print(a.getVal())
-    # a.setValueToAUfromKM()
-    # print(a.getVal())
-    print(a.getTimeOfLightSpeedInMinutesFromTheSun())
-    print(a.getTimeOfLightSpeedInSecondsFromTheSun())
-
-
-
-
-
-
-
-
-
-    # types of the units
-    # types = ["SEC", "MINUTE", "METER", "KM", "AU", "LY", "PC"]
-    #
-    # valueType = 150000000
-    # TimeForSurrunding = 2
-    # DistanceToPlanet = 450*math.pow(10, 6)
-    # Type = types[3]
-    #
-    # eq1 = EquationUtil(types[4], valueType, TimeForSurrunding, DistanceToPlanet)
-    # K = eq1.getKepler()
-    # print(K)
-    # K = eq1.getKeplerBoolean()
-    # if K:
-    #     print("Kepler formula is: {}\n".format(K))
-    # else:
-    #     print("Kepler K is diffrent than 1 {}".format(K))
-    #
-    #
-    #
-    #
-    # ##################################################
-    #
-    # loga, logl, logte = np.loadtxt('isoc_z008.dat', usecols=(0, 3, 4), unpack=True)
-    # w7 = np.where(loga == 7.0)  # 10**7
-    # w7_5 = np.where(loga == 7.5)
-    # w8 = np.where(loga == 8.0)
-    # w8_5 = np.where(loga == 8.5)
-    # w9 = np.where(loga == 9.0)
-    # plt.figure(figsize=[8, 5])
-    #
-    # plt.plot(logte[w7], logl[w7], label='10 Myr')
-    # plt.plot(logte[w8], logl[w8], label='100 Myr')
-    # plt.plot(logte[w7_5], logl[w7_5], label='31.6 Myr')
-    # plt.plot(logte[w8_5], logl[w8_5], label='316.2Myr')
-    # plt.plot(logte[w9], logl[w9], label='1 Byr')
-    #
-    # plt.xlabel('Log(Teff)')
-    # plt.ylabel('Log L')
-    # plt.axis([5.0, 3.5, 0, 6])
-    # plt.title("Isochrones for star cluster with metallicity z =0.008")
-    # plt.legend()
-    # plt.grid()
-    # plt.show()
+    a = EquationUtil("KM", 450000000, 2)
+    # print(a.getTimeOfLightSpeedInMinutesFromTheSun())
+    print(a.getKepler())
